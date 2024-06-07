bcfaaa09bf8610609feafb6fa439e0c2332ddc65
ValentinLuque1 <166860445+ValentinLuque1@users.noreply.github.com>
2024-06-07 15:00:34 -0300

    Add files via upload
    Grupo: Velozo, Luque, Piolo

---
+++ ADC/ADC.h
@@ -0,0 +1,7 @@
+#ifndef ADC_H
+#define ADC_H
+
+void ADC_Init();
+void ADC_Convertion(int channel);
+
+#endif
---
+++ DAC/DAC.h
@@ -0,0 +1,7 @@
+#ifndef DAC_H
+#define DAC_H
+
+void DAC_Init();
+void DAC_Convertion(int valor);
+
+#endif
